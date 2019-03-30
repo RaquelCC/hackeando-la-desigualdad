d7c3237e2008de4ca6e22ee2ab2bb0d8df03fdaf
LeslieSaVa <leslie.sabattin@usach.cl>
2019-03-30 13:03:21 -0300

    pantalla elegir genero

---
+++ src/components/sexCategory.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './sexCategory.css'
+
+function SexCategory (props) {
+    
+    return (
+        <div>
+            <h3 className="title-sexCategory">¿Con qué género te identificas?</h3>
+            <button 
+                className="btn-femele"
+                onClick={props.onClickWomen}
+            >
+            <i className="fas fa-venus"></i></button>
+            <p className="imWoman">Femenino</p>
+            
+            <button 
+                className="btn-male"
+                onClick={props.onClickMan}
+            >
+            <i className="fas fa-mars"></i></button>
+            <p className="imMan">Masculino</p>
+        </div>
+    )
+}
+
+export default SexCategory;
