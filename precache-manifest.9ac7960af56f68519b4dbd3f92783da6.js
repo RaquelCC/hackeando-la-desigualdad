e7596a700f7395000dba1039d6c85e108b993699
RaquelCC <rcanalesconcha@gmail.com>
2019-03-30 17:08:38 -0300

    Updates

---
+++ precache-manifest.9ac7960af56f68519b4dbd3f92783da6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "087bf0ef90ef931eef4b",
+    "url": "/hackeando-la-desigualdad/static/css/main.2fdef7bb.chunk.css"
+  },
+  {
+    "revision": "087bf0ef90ef931eef4b",
+    "url": "/hackeando-la-desigualdad/static/js/main.119ce94a.chunk.js"
+  },
+  {
+    "revision": "9d83b0ce9730b208dd63",
+    "url": "/hackeando-la-desigualdad/static/js/runtime~main.5fe5cb09.js"
+  },
+  {
+    "revision": "7249b539551e1434e381",
+    "url": "/hackeando-la-desigualdad/static/js/2.1c0e75b6.chunk.js"
+  },
+  {
+    "revision": "72208c1befb09414f341c24912a81df9",
+    "url": "/hackeando-la-desigualdad/index.html"
+  }
+];
